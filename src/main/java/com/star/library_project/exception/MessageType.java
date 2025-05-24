8ac4c169c7a3a55b24eecb2ec2c51c7b57127076
Emre Yildiz <yldzemre27@gmail.com>
2025-05-24 21:49:51 +0300

    JWTService ve JWTAuthentication sınıfları oluşturuldu

--- src/main/java/com/star/library_project/exception/MessageType.java
+++ src/main/java/com/star/library_project/exception/MessageType.java
@@ -10,7 +10,8 @@ public enum MessageType {
     USERNAME_NOT_FOUND("1006", "username bulunamadı"),
     USERNAME_OR_PASSWORD_INVALID("1007", "kullanıcı adı veya şifre hatalı"),
     REFRESH_TOKEN_NOT_FOUND("1008", "refresh token bulunamadı"),
-    REFRESH_TOKEN_IS_EXPIRED("1009", "refresh tokenın süresi bitmiştir");
+    REFRESH_TOKEN_IS_EXPIRED("1009", "refresh tokenın süresi bitmiştir"),
+    GENERAL_EXCEPTION("9999", "genel bir hata oluştu");
 
     private String code;
     private String message;
---
+++ src/main/java/com/star/library_project/jwt/JWTService.java
@@ -0,0 +1,57 @@
+package com.star.library_project.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JWTService {
+
+    private static final String SECRET_KEY = "lU97eDQeoeKXcGOc+ADPIFvZSlJjaoFchcOs+gcyIHU=";
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 2))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public Key getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public <T> T exportToken(String token, Function<Claims, T> claimsFunction) {
+        Claims claims = getClaims(token);
+        return claimsFunction.apply(claims);
+    }
+
+    public Claims getClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return exportToken(token, Claims::getSubject);
+    }
+
+    public boolean isTokenValid(String token) {
+        Date expireDate = exportToken(token, Claims::getExpiration);
+        return new Date().before(expireDate);
+    }
+
+}
