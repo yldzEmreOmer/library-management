8643598f8b63316a0d4be2d05b60ad373941df72
Emre Yildiz <yldzemre27@gmail.com>
2025-06-02 23:03:27 +0300

    Service interfaceleri oluşturuldu.

--- src/main/java/com/star/library_project/repository/BookRepository.java
+++ src/main/java/com/star/library_project/repository/BookRepository.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.star.library_project.model.Book;
 import com.star.library_project.model.BookStatus;
